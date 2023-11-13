8905433ab9abc978d7b0f66c80c3753e67cec11f
Andrew <andrewozo123@gmail.com>
2023-11-13 12:54:45 -0500

    finished getImages function

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+async function getImages(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  console.log(data.data[0].url);
+}
+
+console.log(getImages("food"));
